153b4cc74144f6937d76854d445e741847040d33
Chindalath Traymany <chindalatht@gmail.com>
2020-07-21 06:23:42 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,21 +1,21 @@
 # learn spec/artist_spec.rb
 # learn spec/song_spec.rb
 
-class Song
-  attr_accessor :name, :artist
-  @@all = []
+class Post
+  # attr_accessor :name, :artist
+  # @@all = []
   
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
+  # def initialize(name)
+  #   @name = name
+  #   @@all << self
+  # end
 
-  def self.all
-    @@all
-  end
+  # def self.all
+  #   @@all
+  # end
   
-  def artist_name
-    self.artist != nil ? self.artist.name : nil
-  end
+  # def artist_name
+  #   self.artist != nil ? self.artist.name : nil
+  # end
 
 end
