e5cf0c2bbd11c89baa22802fd4a549ebf6391d4e
Chindalath Traymany <chindalatht@gmail.com>
2020-07-21 05:59:58 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+# learn spec/artist_spec.rb
+# learn spec/song_spec.rb
+require 'pry'
+
+class Artist
+  attr_accessor :name, :songs
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def songs
+    Song.all.select { |song| song.artist == self }
+  end
+  
+  def add_song(song)
+    song.artist = self
+  end
+  
+  def add_song_by_name(name)
+    name = Song.new
+    add_song(song)
+  end
+  
+  
+  
+  
+  
+end
